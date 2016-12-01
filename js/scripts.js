b034b67f4121737172249083531b65d000bbffd9
Nigel Burnett and Quinn Schwartz <quinn@>
2016-12-01 11:59:52 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function(){
+        $('.carousel.carousel-slider').carousel({full_width: true});
+        $('.carousel').carousel({dist:0});
+        window.setInterval(function(){$('.carousel').carousel('next')},3000)
+     });
