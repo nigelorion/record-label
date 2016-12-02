0feb6cbac721ebe9b32c92c3ebb6099bd4955c8d
Nigel Burnett and Quinn Schwartz <nigel@>
2016-12-01 16:27:22 -0800

    altered nav bar to include logo image and different background color.

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,5 @@ $(document).ready(function(){
         $('.carousel.carousel-slider').carousel({full_width: true});
         $('.carousel').carousel({dist:0});
         window.setInterval(function(){$('.carousel').carousel('next')},3000)
+          $(".button-collapse").sideNav();
      });
